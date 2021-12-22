fc542ddff0b072c9457175bde43b6c12f5e2d147
Youssef sellami <youssef-sellami@outlook.com>
2021-12-22 21:47:21 +0100

    added the SmsMessage, and phone number entities

---
+++ src/SMS.Net/Models/Message/PhoneNumber.cs
@@ -0,0 +1,38 @@
+﻿namespace SMS.Net
+{
+    using System;
+
+    /// <summary>
+    /// Phone number
+    /// </summary>
+    public class PhoneNumber
+    {
+        private readonly string _number;
+
+        /// <summary>
+        /// Create a new PhoneNumber
+        /// </summary>
+        /// <param name="number">Phone number</param>
+        public PhoneNumber(string number)
+        {
+            if (number is null)
+                throw new ArgumentNullException(nameof(number));
+
+            if (string.IsNullOrEmpty(number))
+                throw new ArgumentException("the given phone number is empty", nameof(number));
+
+            _number = number;
+        }
+
+        /// <summary>
+        /// Add implicit constructor for PhoneNumber to make it assignable from string
+        /// </summary>
+        /// <param name="number">Phone number</param>
+        /// <returns></returns>
+        public static implicit operator PhoneNumber(string number) 
+            => new PhoneNumber(number);
+
+        /// <inheritdoc/>
+        public override string ToString() => _number;
+    }
+}
---
+++ src/SMS.Net/Models/Message/SmsMessage.cs
@@ -0,0 +1,67 @@
+﻿namespace SMS.Net
+{
+    using SMS.Net.Channel;
+    using SMS.Net.Factories;
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// the SMS message.
+    /// </summary>
+    public partial class SmsMessage
+    {
+        /// <summary>
+        /// Gets or sets the priority of this e-mail message.
+        /// </summary>
+        public Priority Priority { get; }
+
+        /// <summary>
+        /// the message body.
+        /// </summary>
+        public string Body { get; set; }
+
+        /// <summary>
+        /// the phone number of the sender.
+        /// </summary>
+        public PhoneNumber From { get; internal set; }
+
+        /// <summary>
+        /// the phone numbers of recipients to send the SMS message to.
+        /// </summary>
+        public ICollection<PhoneNumber> To { get; }
+
+        /// <summary>
+        /// get the collection of additional data need to be passed to the SMS delivery channel for further configuration
+        /// </summary>
+        public ICollection<ChannelData> ChannelData { get; set; }
+    }
+
+    /// <summary>
+    /// partial part for <see cref="SmsMessage"/>
+    /// </summary>
+    public partial class SmsMessage
+    {
+        public SmsMessage(Priority priority, string body, PhoneNumber from, ICollection<PhoneNumber> to, ICollection<ChannelData> channelData)
+        {
+            Priority = priority;
+            Body = body ?? throw new ArgumentNullException(nameof(body));
+            From = from ?? throw new ArgumentNullException(nameof(from));
+            To = to ?? throw new ArgumentNullException(nameof(to));
+            ChannelData = channelData ?? throw new ArgumentNullException(nameof(channelData));
+        }
+
+        /// <summary>
+        /// set the sender phone number.
+        /// </summary>
+        /// <param name="from">the sender phone number.</param>
+        public void SetFrom(PhoneNumber from)
+            => From = from;
+
+        /// <summary>
+        /// create an instance of <see cref="SmsMessageComposer"/> to start composing the message data.
+        /// </summary>
+        /// <returns>instance of <see cref="SmsMessageComposer"/>.</returns>
+        public static SmsMessageComposer Compose()
+            => new SmsMessageComposer();
+    }
+}
