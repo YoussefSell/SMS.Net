b95e004027568cb11aae9b8c941cad12a90d3f57
Youssef sellami <youssef-sellami@outlook.com>
2021-12-22 21:46:47 +0100

    added exceptions

---
+++ src/SMS.Net/Exceptions/SmsDeliveryChannelNotFoundException.cs
@@ -0,0 +1,34 @@
+﻿namespace SMS.Net.Exceptions
+{
+    using System;
+
+    /// <summary>
+    /// exception thrown when no SMS delivery channel has been found
+    /// </summary>
+    [Serializable]
+    public class SmsDeliveryChannelNotFoundException : Exception
+    {
+        private static readonly string message = "there is no SMS delivery channel with the name {{name}}, make sure you have registered the channel with the SMS service";
+
+        /// <summary>
+        /// the name of the SMS delivery channel.
+        /// </summary>
+        public string SmsDeliveryChannelName { get; set; }
+
+        /// <inheritdoc/>
+        public SmsDeliveryChannelNotFoundException(string emailDeliveryProviderName)
+            : base(message.Replace("{{name}}", emailDeliveryProviderName)) { }
+
+        /// <inheritdoc/>
+        public SmsDeliveryChannelNotFoundException(string message, string emailDeliveryProviderName)
+            : base(message)
+        {
+            SmsDeliveryChannelName = emailDeliveryProviderName;
+        }
+
+        /// <inheritdoc/>
+        protected SmsDeliveryChannelNotFoundException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
