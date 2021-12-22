477ff78c7485c5834136f03b6b83c3095cb690d1
Youssef sellami <youssef-sellami@outlook.com>
2021-12-22 21:48:17 +0100

    added utilities classes

---
+++ src/SMS.Net/Utilities/Extensions/ListExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace SMS.Net
+{
+    using SMS.Net.Channel;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// extensions over list data types
+    /// </summary>
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// get the data with the given key.
+        /// </summary>
+        /// <param name="data">the source list.</param>
+        /// <param name="key">the data key</param>
+        /// <returns>the <see cref="ChannelData"/> instance</returns>
+        public static ChannelData GetData(this IEnumerable<ChannelData> data, string key)
+            => data.FirstOrDefault(e => e.Key == key);
+    }
+}
