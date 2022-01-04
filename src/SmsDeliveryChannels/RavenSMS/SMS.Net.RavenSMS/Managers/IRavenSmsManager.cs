ed1ad383644c07ff1a76bd78507233c06c5341b6
Youssef sellami <youssef-sellami@outlook.com>
2022-01-04 21:45:15 +0100

    added 2 function to queue the RavenSMS messages

--- src/SmsDeliveryChannels/RavenSMS/SMS.Net.RavenSMS/Managers/IRavenSmsManager.cs
+++ src/SmsDeliveryChannels/RavenSMS/SMS.Net.RavenSMS/Managers/IRavenSmsManager.cs
@@ -1,6 +1,27 @@
 ﻿namespace SMS.Net.RavenSMS.Managers
 {
+    using SMS.Net.Channel.RavenSMS;
+    using System.Threading.Tasks;
+    using System;
+
+    /// <summary>
+    /// the manager for managing SMS messages.
+    /// </summary>
     public interface IRavenSmsManager
     {
+        /// <summary>
+        /// queue the message for processing
+        /// </summary>
+        /// <param name="ravenSmsMessage">the message to queue</param>
+        /// <returns>a Task instance</returns>
+        Task QueueMessageAsync(RavenSmsMessage ravenSmsMessage);
+
+        /// <summary>
+        /// queue the message for processing
+        /// </summary>
+        /// <param name="ravenSmsMessage">the message to queue.</param>
+        /// <param name="delay">the delay to use before sending the message.</param>
+        /// <returns>a Task instance</returns>
+        Task QueueMessageAsync(RavenSmsMessage ravenSmsMessage, TimeSpan delay);
     }
 }
--- src/SmsDeliveryChannels/RavenSMS/SMS.Net.RavenSMS/Managers/RavenSmsManager.cs
+++ src/SmsDeliveryChannels/RavenSMS/SMS.Net.RavenSMS/Managers/RavenSmsManager.cs
@@ -1,10 +1,22 @@
 ﻿namespace SMS.Net.RavenSMS.Managers
 {
+    using SMS.Net.Channel.RavenSMS;
+    using System;
+    using System.Threading.Tasks;
+
     /// <summary>
     /// the ravenSMS manager, used to manage all messages sent with RavenSMS 
     /// </summary>
     public partial class RavenSmsManager : IRavenSmsManager
     {
+        public Task QueueMessageAsync(RavenSmsMessage ravenSmsMessage)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task QueueMessageAsync(RavenSmsMessage ravenSmsMessage, TimeSpan delay)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
