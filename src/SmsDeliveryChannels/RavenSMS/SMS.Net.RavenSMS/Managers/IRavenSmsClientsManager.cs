6c204bc1db993ed3a44181de47520efcd022819a
Youssef sellami <youssef-sellami@outlook.com>
2022-01-04 21:44:50 +0100

    added a manager for managing ravenSMS clients

---
+++ src/SmsDeliveryChannels/RavenSMS/SMS.Net.RavenSMS/Managers/IRavenSmsClientsManager.cs
@@ -0,0 +1,19 @@
+﻿namespace SMS.Net.RavenSMS.Managers
+{
+    using System;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// the manager for managing the RavenSMS client apps
+    /// </summary>
+    public interface IRavenSmsClientsManager
+    {
+        /// <summary>
+        /// check if there is any Client app registered with the given phone number
+        /// </summary>
+        /// <param name="from">the phone number instance</param>
+        /// <returns>true if exist, false if not</returns>
+        /// <exception cref="ArgumentNullException">if the <paramref name="from"/> value is null</exception>
+        Task<bool> AnyAsync(PhoneNumber from);
+    }
+}
