484b48a10b09aa5130d4a1db4224d9460356fe84
Youssef sellami <youssef-sellami@outlook.com>
2022-01-05 21:01:24 +0100

    added implementation for IRavenSmsClientsManager

---
+++ src/SmsDeliveryChannels/RavenSMS/SMS.Net.RavenSMS/Managers/RavenSmsClientsManager/RavenSmsClientsManager.cs
@@ -0,0 +1,13 @@
+﻿namespace SMS.Net.RavenSMS.Managers
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public class RavenSmsClientsManager : IRavenSmsClientsManager
+    {
+        public Task<bool> AnyAsync(PhoneNumber from)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
