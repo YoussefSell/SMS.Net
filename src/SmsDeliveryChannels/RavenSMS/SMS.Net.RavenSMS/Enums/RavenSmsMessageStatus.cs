b605073d68546e4b2710e9df0d0ad6b118ce77cb
Youssef sellami <youssef-sellami@outlook.com>
2022-01-05 21:44:23 +0100

    added message status enum

---
+++ src/SmsDeliveryChannels/RavenSMS/SMS.Net.RavenSMS/Enums/RavenSmsMessageStatus.cs
@@ -0,0 +1,27 @@
+﻿namespace SMS.Net.Channel.RavenSMS.Enums;
+
+/// <summary>
+/// the status of the message.
+/// </summary>
+public enum RavenSmsMessageStatus
+{
+    /// <summary>
+    /// the message has been created.
+    /// </summary>
+    Created,
+
+    /// <summary>
+    /// the message has been Queued.
+    /// </summary>
+    Queued,
+
+    /// <summary>
+    /// failed to send the message.
+    /// </summary>
+    Failed,
+
+    /// <summary>
+    /// the message has been sent successfully.
+    /// </summary>
+    Sent,
+}
