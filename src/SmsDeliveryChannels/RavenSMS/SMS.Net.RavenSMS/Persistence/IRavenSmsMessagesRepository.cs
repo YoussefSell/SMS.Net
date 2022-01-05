00cd202666bfa648960741771cdcbb010dc2ff5a
Youssef sellami <youssef-sellami@outlook.com>
2022-01-05 21:17:51 +0100

    added a repository for the RavenSMS messages data management

---
+++ src/SmsDeliveryChannels/RavenSMS/SMS.Net.RavenSMS/Persistence/IRavenSmsMessagesRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace SMS.Net.Channel.RavenSMS.Persistence;
+
+/// <summary>
+/// the repository for managing SMS messages data
+/// </summary>
+public interface IRavenSmsMessagesRepository
+{
+}
