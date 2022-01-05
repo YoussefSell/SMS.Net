0b1e6c490a48d69c9bd6ef4680663374db5b23d9
Youssef sellami <youssef-sellami@outlook.com>
2022-01-05 21:47:11 +0100

    added some methods to the IRavenSmsMessagesRepository

--- src/SmsDeliveryChannels/RavenSMS/SMS.Net.RavenSMS/Persistence/IRavenSmsMessagesRepository.cs
+++ src/SmsDeliveryChannels/RavenSMS/SMS.Net.RavenSMS/Persistence/IRavenSmsMessagesRepository.cs
@@ -5,4 +5,24 @@
 /// </summary>
 public interface IRavenSmsMessagesRepository
 {
+    /// <summary>
+    /// find the message with the given id.
+    /// </summary>
+    /// <param name="messageId">the id of the message</param>
+    /// <returns>the message, or null if not found</returns>
+    Task<RavenSmsMessage?> FindByIdAsync(Guid messageId);
+
+    /// <summary>
+    /// save the given message.
+    /// </summary>
+    /// <param name="message">the message to be saved</param>
+    /// <returns>the operation result</returns>
+    Task<Result> SaveAsync(RavenSmsMessage message);
+
+    /// <summary>
+    /// update the given message.
+    /// </summary>
+    /// <param name="message">the message to be updated</param>
+    /// <returns>the operation result</returns>
+    Task<Result> UpdateAsync(RavenSmsMessage message);
 }
