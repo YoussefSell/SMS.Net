a973f040a232d28051ee7240f136b0f7b557ebb6
Youssef sellami <youssef-sellami@outlook.com>
2022-01-05 21:48:05 +0100

    added methods to the IQueueManager

--- src/SmsDeliveryChannels/RavenSMS/SMS.Net.RavenSMS/Managers/IQueueManager.cs
+++ src/SmsDeliveryChannels/RavenSMS/SMS.Net.RavenSMS/Managers/IQueueManager.cs
@@ -5,4 +5,18 @@
 /// </summary>
 public interface IQueueManager
 {
+    /// <summary>
+    /// queue the message for processing
+    /// </summary>
+    /// <param name="message">the message to queue</param>
+    /// <returns>the id of the queue job</returns>
+    Task<string> QueueMessageAsync(RavenSmsMessage message);
+
+    /// <summary>
+    /// queue the message for processing
+    /// </summary>
+    /// <param name="message">the message to queue.</param>
+    /// <param name="delay">the delay to use before sending the message.</param>
+    /// <returns>a Task instance</returns>
+    Task<string> QueueMessageAsync(RavenSmsMessage message, TimeSpan delay);
 }
