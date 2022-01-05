99d9d086b6133473210fe1b95fc5b7d92a7a2058
Youssef sellami <youssef-sellami@outlook.com>
2022-01-05 20:52:05 +0100

    added an extension method on the SmsMessageComposer to set the delay value for RavenSMS

--- src/SmsDeliveryChannels/RavenSMS/SMS.Net.RavenSMS/Extensions/RavenSmsSmsMessageComposerExtensions.cs
+++ src/SmsDeliveryChannels/RavenSMS/SMS.Net.RavenSMS/Extensions/RavenSmsSmsMessageComposerExtensions.cs
@@ -3,13 +3,19 @@
     using SMS.Net.Channel.RavenSMS;
     using SMS.Net.Factories;
     using System;
-    using System.Collections.Generic;
 
     /// <summary>
     /// the extensions methods over the <see cref="SmsMessageComposer"/> factory.
     /// </summary>
     public static class RavenSmsSmsMessageComposerExtensions
     {
-        
+        /// <summary>
+        /// set the delay to wait before sending the message
+        /// </summary>
+        /// <param name="messageComposer">the message composer instance.</param>
+        /// <param name="delay">the Timespan delay.</param>
+        /// <returns>Instance of <see cref="SmsMessageComposer"/> to enable fluent chaining.</returns>
+        public static SmsMessageComposer SendAfter(this SmsMessageComposer messageComposer, TimeSpan delay)
+            => messageComposer.PassChannelData(CustomChannelData.Delay, delay);
     }
 }
