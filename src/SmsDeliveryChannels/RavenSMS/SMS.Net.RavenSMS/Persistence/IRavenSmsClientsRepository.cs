b1c6f95d4b6344cdba53d8b49253f4918e4acdf4
Youssef sellami <youssef-sellami@outlook.com>
2022-01-06 23:29:04 +0100

    added a function to check if the client exist by phone number

--- src/SmsDeliveryChannels/RavenSMS/SMS.Net.RavenSMS/Persistence/IRavenSmsClientsRepository.cs
+++ src/SmsDeliveryChannels/RavenSMS/SMS.Net.RavenSMS/Persistence/IRavenSmsClientsRepository.cs
@@ -5,5 +5,11 @@
 /// </summary>
 public interface IRavenSmsClientsRepository
 {
-
+    /// <summary>
+    /// check if there is any client with the given phone number.
+    /// </summary>
+    /// <param name="from">the phone number instance.</param>
+    /// <returns>true if exist, false if not.</returns>
+    /// <exception cref="ArgumentNullException">if the given phone number instance is null.</exception>
+    Task<bool> AnyAsync(PhoneNumber from);
 }
