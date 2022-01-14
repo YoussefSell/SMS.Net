3a029b9fee3b5ef5956041a7608e7964591d981e
Youssef sellami <youssef-sellami@outlook.com>
2022-01-14 23:00:36 +0100

    added store implementation with ravenSmsDbContext

---
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS.EntityFramework/Stores/RavenSmsClientsStore.cs
@@ -0,0 +1,36 @@
+﻿namespace SMS.Net.Channel.RavenSMS.EntityFramework;
+
+/// <summary>
+/// the store implementation for <see cref="IRavenSmsClientsStore"/>
+/// </summary>
+public partial class RavenSmsClientsStore : IRavenSmsClientsStore
+{
+    /// <inheritdoc/>
+    public Task<bool> AnyAsync(PhoneNumber from) 
+        => _context.Clients.AnyAsync(client => client.PhoneNumbers.Contains(from.ToString()));
+
+    /// <inheritdoc/>
+    public Task<RavenSmsClient?> FindByIdAsync(Guid clientId)
+        => _context.Clients.FirstOrDefaultAsync(client => client.Id == clientId);
+
+    /// <inheritdoc/>
+    public Task<RavenSmsClient?> FindByPhoneNumberAsync(PhoneNumber phoneNumber)
+        => _context.Clients.FirstOrDefaultAsync(client => client.PhoneNumbers.Contains(phoneNumber.ToString()));
+
+    /// <inheritdoc/>
+    public Task<RavenSmsClient[]> GetAllAsync()
+        => _context.Clients.ToArrayAsync();
+}
+
+/// <summary>
+/// partial part for <see cref="RavenSmsClientsStore"/>
+/// </summary>
+public partial class RavenSmsClientsStore
+{
+    private readonly IRavenSmsDbContext _context;
+
+    public RavenSmsClientsStore(IRavenSmsDbContext context)
+    {
+        _context = context;
+    }
+}
---
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS.EntityFramework/Stores/RavenSmsMessagesStore.cs
@@ -0,0 +1,58 @@
+﻿namespace SMS.Net.Channel.RavenSMS.EntityFramework;
+
+/// <summary>
+/// the store implementation for <see cref="IRavenSmsMessagesStore"/>
+/// </summary>
+public partial class RavenSmsMessagesStore : IRavenSmsMessagesStore
+{
+    /// <inheritdoc/>
+    public Task<RavenSmsMessage?> FindByIdAsync(Guid messageId)
+        => _context.Messages.FirstOrDefaultAsync(message => message.Id == messageId);
+
+    /// <inheritdoc/>
+    public async Task<Result<RavenSmsMessage>> SaveAsync(RavenSmsMessage message)
+    {
+        try
+        {
+            var entity = _context.Messages.Add(message);
+            await _context.SaveChangesAsync();
+            return entity.Entity;
+        }
+        catch (Exception ex)
+        {
+            return Result.Failure<RavenSmsMessage>()
+                .WithMessage("Failed to save the message, an exception has been accrued")
+                .WithErrors(ex);
+        }
+    }
+
+    /// <inheritdoc/>
+    public async Task<Result<RavenSmsMessage>> UpdateAsync(RavenSmsMessage message)
+    {
+        try
+        {
+            var entity = _context.Messages.Update(message);
+            await _context.SaveChangesAsync();
+            return entity.Entity;
+        }
+        catch (Exception ex)
+        {
+            return Result.Failure<RavenSmsMessage>()
+                .WithMessage("Failed to update the message, an exception has been accrued")
+                .WithErrors(ex);
+        }
+    }
+}
+
+/// <summary>
+/// partial part for <see cref="RavenSmsClientsStore"/>
+/// </summary>
+public partial class RavenSmsMessagesStore
+{
+    private readonly IRavenSmsDbContext _context;
+
+    public RavenSmsMessagesStore(IRavenSmsDbContext context)
+    {
+        _context = context;
+    }
+}
