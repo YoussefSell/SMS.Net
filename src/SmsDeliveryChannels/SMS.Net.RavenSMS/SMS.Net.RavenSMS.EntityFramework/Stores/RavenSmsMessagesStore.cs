1254d4af3e3eaab510a25ee8be1aea611f299394
Youssef sellami <youssef-sellami@outlook.com>
2022-01-15 23:20:37 +0100

    added implementation for the functions of retrieving the list of messages

--- src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS.EntityFramework/Stores/RavenSmsMessagesStore.cs
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS.EntityFramework/Stores/RavenSmsMessagesStore.cs
@@ -10,6 +10,40 @@ public partial class RavenSmsMessagesStore : IRavenSmsMessagesStore
         => _context.Messages.FirstOrDefaultAsync(message => message.Id == messageId);
 
     /// <inheritdoc/>
+    public Task<RavenSmsMessage[]> GetAllAsync()
+        => _context.Messages.ToArrayAsync();
+
+    /// <inheritdoc/>
+    public async Task<(RavenSmsMessage[] data, int rowsCount)> GetAllAsync(RavenSmsMessageFilter filter)
+    {
+        var query = _context.Messages.AsQueryable();
+
+        // apply the filter & the orderBy
+        query = SetFilter(query, filter);
+        query = query.DynamicOrderBy(filter.OrderBy, filter.SortDirection);
+
+        var rowsCount = 0;
+
+        if (!filter.IgnorePagination)
+        {
+            rowsCount = await query.Select(e => e.Id)
+                .Distinct()
+                .CountAsync();
+
+            query = query.Skip((filter.PageIndex - 1) * filter.PageSize)
+                .Take(filter.PageSize);
+        }
+
+        var data = await query.ToArrayAsync();
+
+        rowsCount = filter.IgnorePagination
+            ? data.Length
+            : rowsCount;
+
+        return (data, rowsCount);
+    }
+
+    /// <inheritdoc/>
     public async Task<Result<RavenSmsMessage>> SaveAsync(RavenSmsMessage message)
     {
         try
@@ -54,5 +88,34 @@ public partial class RavenSmsMessagesStore
     public RavenSmsMessagesStore(IRavenSmsDbContext context)
     {
         _context = context;
+    }
+
+    private static IQueryable<RavenSmsMessage> SetFilter(IQueryable<RavenSmsMessage> query, RavenSmsMessageFilter filter)
+    {
+        if (!string.IsNullOrEmpty(filter.SearchQuery))
+            query = query.Where(e => EF.Functions.Like(e.Body, $"%{filter.SearchQuery}%"));
+
+        if (filter.StartDate.HasValue)
+            query = query.Where(e => e.CreateOn >= filter.StartDate);
+
+        if (filter.EndDate.HasValue)
+            query = query.Where(e => e.CreateOn >= filter.EndDate);
+
+        if (filter.Priority.HasValue)
+            query = query.Where(e => e.Priority == filter.Priority);
+
+        if (filter.Status is not null && filter.Status.Any())
+            query = query.Where(e => filter.Status.Contains(e.Status));
+
+        if (filter.To is not null && filter.To.Any())
+            query = query.Where(e => filter.To.Contains((string)e.To));
+
+        if (filter.From is not null && filter.From.Any())
+            query = query.Where(e => filter.From.Contains((string)e.From));
+
+        if (filter.Clients is not null && filter.Clients.Any())
+            query = query.Where(e => filter.Clients.Contains(e.ClientId));
+
+        return query;
     }
 }
