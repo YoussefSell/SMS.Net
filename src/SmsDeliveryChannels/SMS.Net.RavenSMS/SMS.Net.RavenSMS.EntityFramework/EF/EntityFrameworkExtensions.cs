b5cab6200dd1a524e1b07909ef2a303e26cb5da5
Youssef sellami <youssef-sellami@outlook.com>
2022-01-15 21:55:02 +0100

    added extension method to configure RavenSMS entities with EF

---
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS.EntityFramework/EF/EntityFrameworkExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace SMS.Net.Channel.RavenSMS.EntityFramework;
+
+/// <summary>
+/// extension for EF core configuration
+/// </summary>
+public static class EntityFrameworkExtensions
+{
+    /// <summary>
+    /// Apply the Commune Configuration to all entities that derives from <see cref="Entity{Tkey}"/>
+    /// </summary>
+    /// <param name="modelBuilder">the module builder instance</param>
+    /// <returns>the module builder instance</returns>
+    public static ModelBuilder ApplyRavenSmsEntityConfiguration(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new RavenSmsClientEntityConfiguration());
+        modelBuilder.ApplyConfiguration(new RavenSmsMessageEntityConfiguration());
+
+        return modelBuilder;
+    }
+}
+
