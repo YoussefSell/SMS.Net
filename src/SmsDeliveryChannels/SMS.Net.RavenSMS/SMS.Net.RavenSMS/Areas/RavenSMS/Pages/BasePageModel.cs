e36cfb0d879587b3e38ab2d658e974b664927892
Youssef sellami <youssef-sellami@outlook.com>
2022-01-17 22:00:26 +0100

    added a base page model class

---
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Areas/RavenSMS/Pages/BasePageModel.cs
@@ -0,0 +1,37 @@
+﻿namespace SMS.Net.RavenSMS.Pages;
+
+/// <summary>
+/// the base page model
+/// </summary>
+public class BasePageModel : PageModel
+{
+    protected readonly IStringLocalizer _localizer;
+    protected readonly ILogger _logger;
+
+    public BasePageModel(IStringLocalizer localizer, ILogger logger)
+    {
+        _logger = logger;
+        _localizer = localizer;
+    }
+
+    /// <summary>
+    /// the status message a TempData used to share alert messages between pages
+    /// </summary>
+    [TempData]
+    public string? StatusMessage { get; set; }
+
+    /// <summary>
+    /// translate the given name string, using the provided translation implantation
+    /// </summary>
+    /// <param name="name">the name to localize</param>
+    /// <param name="arguments">the args if any</param>
+    /// <returns>the translated text</returns>
+    public virtual string Localize(string name, params object[] arguments)
+    {
+        var translation = _localizer[name, arguments];
+        if (translation is not null && !translation.ResourceNotFound)
+            return translation.Value;
+
+        return name;
+    }
+}
