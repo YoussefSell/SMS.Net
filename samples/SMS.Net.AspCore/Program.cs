f9d0f3e6430f9e00466362a9b87f3103843b677c
Youssef sellami <youssef-sellami@outlook.com>
2022-01-18 21:39:09 +0100

    added localization

--- samples/SMS.Net.AspCore/Program.cs
+++ samples/SMS.Net.AspCore/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddSMSNet(options =>
     options.UseEntityFrameworkStores<ApplicationDbContext>();
 });
 
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
