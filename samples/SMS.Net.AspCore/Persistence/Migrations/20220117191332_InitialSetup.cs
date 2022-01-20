f906eff07df5e8d9bc1d4fe410936e3edf723fda
Youssef sellami <youssef-sellami@outlook.com>
2022-01-20 22:46:46 +0100

    added ef core migrations

---
+++ samples/SMS.Net.AspCore/Persistence/Migrations/20220117191332_InitialSetup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SMS.Net.AspCore.Migrations
+{
+    public partial class InitialSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RavenSmsClients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 150, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 300, nullable: false),
+                    CreatedOn = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    PhoneNumbers = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RavenSmsClients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RavenSmsMessages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreateOn = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    Priority = table.Column<string>(type: "TEXT", nullable: false),
+                    Body = table.Column<string>(type: "TEXT", maxLength: 500, nullable: false),
+                    To = table.Column<string>(type: "TEXT", nullable: false),
+                    From = table.Column<string>(type: "TEXT", nullable: false),
+                    JobQueueId = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    ClientId = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RavenSmsMessages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RavenSmsMessages_RavenSmsClients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "RavenSmsClients",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RavenSmsMessages_ClientId",
+                table: "RavenSmsMessages",
+                column: "ClientId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RavenSmsMessages");
+
+            migrationBuilder.DropTable(
+                name: "RavenSmsClients");
+        }
+    }
+}
