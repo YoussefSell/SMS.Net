219037b0d5f65285a50c371a7f21bf32f8770783
Youssef sellami <youssef-sellami@outlook.com>
2022-01-22 19:11:08 +0100

    added the MessagesEditPageModel

--- src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Areas/RavenSMS/Pages/Messages/Edit.cshtml.cs
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Areas/RavenSMS/Pages/Messages/Edit.cshtml.cs
@@ -1,9 +1,26 @@
-﻿namespace SMS.Net.RavenSMS.Pages
+﻿namespace SMS.Net.RavenSMS.Pages;
+
+/// <summary>
+/// the Messages edit pages
+/// </summary>
+public partial class MessagesEditPageModel
 {
-    /// <summary>
-    /// the Messages edit pages
-    /// </summary>
-    public class MessagesEditPage
+}
+
+/// <summary>
+/// partial part for <see cref="MessagesEditPageModel"/>
+/// </summary>
+public partial class MessagesEditPageModel : BasePageModel
+{
+    private readonly IRavenSmsManager _manager;
+
+    public MessagesEditPageModel(
+        IRavenSmsManager ravenSmsManager,
+        IStringLocalizer<MessagesAddPageModel> localizer,
+        ILogger<MessagesAddPageModel> logger)
+        : base(localizer, logger)
     {
+        _manager = ravenSmsManager;
     }
 }
+
