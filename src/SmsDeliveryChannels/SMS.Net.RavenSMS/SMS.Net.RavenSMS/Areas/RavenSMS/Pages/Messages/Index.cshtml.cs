7f4343d10ccd3b01df8582f2e276193917c276c2
Youssef sellami <youssef-sellami@outlook.com>
2022-01-23 16:07:41 +0100

    update page model for the message index page

--- src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Areas/RavenSMS/Pages/Messages/Index.cshtml.cs
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Areas/RavenSMS/Pages/Messages/Index.cshtml.cs
@@ -1,9 +1,25 @@
-﻿namespace SMS.Net.RavenSMS.Pages
+﻿namespace SMS.Net.RavenSMS.Pages;
+
+/// <summary>
+/// the Messages index pages
+/// </summary>
+public partial class MessagesIndexPageModel
 {
-    /// <summary>
-    /// the Messages index pages
-    /// </summary>
-    public class MessagesIndexPage
+}
+
+/// <summary>
+/// partial part for <see cref="MessagesIndexPageModel"/>
+/// </summary>
+public partial class MessagesIndexPageModel : BasePageModel
+{
+    private readonly IRavenSmsManager _manager;
+
+    public MessagesIndexPageModel(
+        IRavenSmsManager ravenSmsManager,
+        IStringLocalizer<MessagesAddPageModel> localizer,
+        ILogger<MessagesAddPageModel> logger)
+        : base(localizer, logger)
     {
+        _manager = ravenSmsManager;
     }
 }
