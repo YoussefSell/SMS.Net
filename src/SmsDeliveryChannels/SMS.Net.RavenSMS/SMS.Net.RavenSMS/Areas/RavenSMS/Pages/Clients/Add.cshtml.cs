c28a25982d33af1087a131ba29cb76acaa6960f1
Youssef sellami <youssef-sellami@outlook.com>
2022-01-23 16:14:25 +0100

    update Clients Add PageModel

--- src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Areas/RavenSMS/Pages/Clients/Add.cshtml.cs
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Areas/RavenSMS/Pages/Clients/Add.cshtml.cs
@@ -1,9 +1,25 @@
-﻿namespace SMS.Net.RavenSMS.Pages
+﻿namespace SMS.Net.RavenSMS.Pages;
+
+/// <summary>
+/// the Clients add page
+/// </summary>
+public partial class ClientsAddPageModel
 {
-    /// <summary>
-    /// the Clients add page
-    /// </summary>
-    public class ClientsAddPage
+}
+
+/// <summary>
+/// partial part for <see cref="ClientsAddPageModel"/>
+/// </summary>
+public partial class ClientsAddPageModel : BasePageModel
+{
+    private readonly IRavenSmsManager _manager;
+
+    public ClientsAddPageModel(
+        IRavenSmsManager ravenSmsManager,
+        IStringLocalizer<MessagesAddPageModel> localizer,
+        ILogger<MessagesAddPageModel> logger)
+        : base(localizer, logger)
     {
+        _manager = ravenSmsManager;
     }
 }
