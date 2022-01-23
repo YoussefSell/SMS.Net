e43540788bb8af4318b9faea2c5bd33210b29d43
Youssef sellami <youssef-sellami@outlook.com>
2022-01-23 16:08:56 +0100

    updated page model for messages preview page

--- src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Areas/RavenSMS/Pages/Messages/Preview.cshtml.cs
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Areas/RavenSMS/Pages/Messages/Preview.cshtml.cs
@@ -1,9 +1,25 @@
-﻿namespace SMS.Net.RavenSMS.Pages
+﻿namespace SMS.Net.RavenSMS.Pages;
+
+/// <summary>
+/// the Messages Preview pages
+/// </summary>
+public partial class MessagesPreviewPageModel
 {
-    /// <summary>
-    /// the Messages Preview pages
-    /// </summary>
-    public class MessagesPreviewPage
+}
+
+/// <summary>
+/// partial part for <see cref="MessagesPreviewPageModel"/>
+/// </summary>
+public partial class MessagesPreviewPageModel : BasePageModel
+{
+    private readonly IRavenSmsManager _manager;
+
+    public MessagesPreviewPageModel(
+        IRavenSmsManager ravenSmsManager,
+        IStringLocalizer<MessagesAddPageModel> localizer,
+        ILogger<MessagesAddPageModel> logger)
+        : base(localizer, logger)
     {
+        _manager = ravenSmsManager;
     }
 }
