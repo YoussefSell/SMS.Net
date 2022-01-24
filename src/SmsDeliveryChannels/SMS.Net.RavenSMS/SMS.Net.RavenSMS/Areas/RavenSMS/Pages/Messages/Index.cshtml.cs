81b070433c9497bffd7e757aa66c0234a82163c6
Youssef sellami <youssef-sellami@outlook.com>
2022-01-24 21:00:10 +0100

    added an action to return list of messages as a json response

--- src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Areas/RavenSMS/Pages/Messages/Index.cshtml.cs
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Areas/RavenSMS/Pages/Messages/Index.cshtml.cs
@@ -10,6 +10,33 @@ public partial class MessagesIndexPageModel
 /// <summary>
 /// partial part for <see cref="MessagesIndexPageModel"/>
 /// </summary>
+public partial class MessagesIndexPageModel
+{
+    public async Task<JsonResult> OnGetMessagesAsync()
+    {
+        var (messages, rowsCount) = await _manager.GetAllMessagesAsync();
+
+        return new JsonResult(new {
+            rowsCount,
+            data = messages.Select(message => new
+            {
+                message.Id,
+                message.From,
+                message.To,
+                message.Status,
+                client = new
+                {
+                    message.Client?.Id,
+                    message.Client?.Name,
+                },
+            }),
+        });
+    }
+}
+
+/// <summary>
+/// partial part for <see cref="MessagesIndexPageModel"/>
+/// </summary>
 public partial class MessagesIndexPageModel : BasePageModel
 {
     private readonly IRavenSmsManager _manager;
