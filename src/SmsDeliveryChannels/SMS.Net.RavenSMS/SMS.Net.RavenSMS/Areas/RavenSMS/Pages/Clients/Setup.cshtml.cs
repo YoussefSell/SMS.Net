d10b0601e7b9c2b14d47022a6f5ac1411d8440ff
Youssef sellami <youssef-sellami@outlook.com>
2022-02-05 18:31:53 +0100

    added the logic for retrieving the client to setup

--- src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Areas/RavenSMS/Pages/Clients/Setup.cshtml.cs
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Areas/RavenSMS/Pages/Clients/Setup.cshtml.cs
@@ -5,7 +5,38 @@
 /// </summary>
 public partial class ClientSetupPageModel
 {
-    
+    /// <summary>
+    /// Get or set the message.
+    /// </summary>
+    public string? Message { get; set; }
+
+    /// <summary>
+    /// Get or set the client to setup.
+    /// </summary>
+    public RavenSmsClient? Client { get; set; }
+}
+
+/// <summary>
+/// partial part for <see cref="ClientSetupPageModel"/>
+/// </summary>
+public partial class ClientSetupPageModel
+{
+    public async Task<IActionResult> OnGetAsync(string? id)
+    {
+        if (string.IsNullOrEmpty(id))
+            return RedirectToPage("/Clients/index", new { area = "RavenSMS" });
+
+        var client = await _manager.FindClientByIdAsync(id);
+        if (client is null)
+        {
+            Message = $"Couldn't find a client with the Id: {id}";
+            return Page();
+        }
+
+        Client = client;
+
+        return Page();
+    }
 }
 
 /// <summary>
