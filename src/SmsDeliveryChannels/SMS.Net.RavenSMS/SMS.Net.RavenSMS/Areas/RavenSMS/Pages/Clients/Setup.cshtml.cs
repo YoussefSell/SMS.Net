8612a5db74d55bfc4b855e9da2dc5431eea60373
Youssef sellami <youssef-sellami@outlook.com>
2022-02-05 18:04:36 +0100

    added client setup page

---
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Areas/RavenSMS/Pages/Clients/Setup.cshtml.cs
@@ -0,0 +1,27 @@
+﻿namespace SMS.Net.RavenSMS.Pages;
+
+/// <summary>
+/// the Messages index pages
+/// </summary>
+public partial class ClientSetupPageModel
+{
+    
+}
+
+/// <summary>
+/// partial part for <see cref="ClientSetupPageModel"/>
+/// </summary>
+public partial class ClientSetupPageModel : BasePageModel
+{
+    private readonly IRavenSmsManager _manager;
+
+    public ClientSetupPageModel(
+        IRavenSmsManager ravenSmsManager,
+        IStringLocalizer<ClientSetupPageModel> localizer,
+        ILogger<ClientSetupPageModel> logger)
+        : base(localizer, logger)
+    {
+        _manager = ravenSmsManager;
+    }
+}
+
