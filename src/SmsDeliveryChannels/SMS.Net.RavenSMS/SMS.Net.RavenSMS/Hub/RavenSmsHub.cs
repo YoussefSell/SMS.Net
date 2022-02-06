90943ca0aafc923961415f7d49a68b8ff2ac8c56
Youssef sellami <youssef-sellami@outlook.com>
2022-02-06 19:54:10 +0100

    added ravenSms hub signalR hub

---
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Hub/RavenSmsHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SMS.Net.Channel.RavenSMS;
+
+public class RavenSmsHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("SendMessage", user, message);
+    }
+}
