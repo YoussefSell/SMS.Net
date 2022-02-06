ccfb722d98790d72241404ee08b64fe2b6150704
Youssef sellami <youssef-sellami@outlook.com>
2022-02-06 19:59:13 +0100

    added the logic for retrieving a client in preview page

--- src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Areas/RavenSMS/Pages/Clients/Preview.cshtml.cs
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Areas/RavenSMS/Pages/Clients/Preview.cshtml.cs
@@ -1,9 +1,57 @@
-﻿namespace SMS.Net.RavenSMS.Pages
+﻿namespace SMS.Net.RavenSMS.Pages;
+
+/// <summary>
+/// the Messages index pages
+/// </summary>
+public partial class ClientsPreviewPage
 {
     /// <summary>
-    /// the Clients Preview pages
+    /// Get or set the message.
     /// </summary>
-    public class ClientsPreviewPage
+    public string? Message { get; set; }
+
+    /// <summary>
+    /// Get or set the client to setup.
+    /// </summary>
+    public RavenSmsClient? Client { get; set; }
+}
+
+/// <summary>
+/// partial part for <see cref="ClientsPreviewPage"/>
+/// </summary>
+public partial class ClientsPreviewPage
+{
+    public async Task<IActionResult> OnGetAsync(string? id)
     {
+        if (string.IsNullOrEmpty(id))
+            return RedirectToPage("/Clients/index", new { area = "RavenSMS" });
+
+        var client = await _manager.FindClientByIdAsync(id);
+        if (client is null)
+        {
+            Message = $"Couldn't find a client with the Id: {id}";
+            return Page();
+        }
+
+        Client = client;
+
+        return Page();
+    }
+}
+
+/// <summary>
+/// partial part for <see cref="ClientsPreviewPage"/>
+/// </summary>
+public partial class ClientsPreviewPage : BasePageModel
+{
+    private readonly IRavenSmsManager _manager;
+
+    public ClientsPreviewPage(
+        IRavenSmsManager ravenSmsManager,
+        IStringLocalizer<ClientsPreviewPage> localizer,
+        ILogger<ClientsPreviewPage> logger)
+        : base(localizer, logger)
+    {
+        _manager = ravenSmsManager;
     }
 }
