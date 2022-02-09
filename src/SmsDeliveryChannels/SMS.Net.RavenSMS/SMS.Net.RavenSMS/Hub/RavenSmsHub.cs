b072a1a128b5392c9813599241fdfb57a5a3a7b4
Youssef sellami <youssef-sellami@outlook.com>
2022-02-09 22:03:03 +0100

    removed unused methods

--- src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Hub/RavenSmsHub.cs
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Hub/RavenSmsHub.cs
@@ -4,17 +4,5 @@ namespace SMS.Net.Channel.RavenSMS;
 
 public class RavenSmsHub : Hub
 {
-    public override async Task OnConnectedAsync()
-    {
-        await Task.Delay(1);
-
-        var context = this.Context;
-        var context1 = this.Clients;
-        var context2 = this.Groups;
-    }
-
-    public async Task SendMessage(string user, string message)
-    {
-        await Clients.All.SendAsync("SendMessage", user, message);
-    }
+   
 }
