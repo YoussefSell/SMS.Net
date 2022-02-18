17587aa6616badad4c5670c0576fe43dc2b952c6
Youssef sellami <youssef-sellami@outlook.com>
2022-02-18 23:24:01 +0100

    added a new interface to define ravenSMS client connection

---
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS/Hub/IRavenSmsClientConnector.cs
@@ -0,0 +1,8 @@
+﻿namespace SMS.Net.Channel.RavenSMS;
+
+/// <summary>
+/// the ravenSMS client connector to send commands to the client
+/// </summary>
+public interface IRavenSmsClientConnector
+{
+}
