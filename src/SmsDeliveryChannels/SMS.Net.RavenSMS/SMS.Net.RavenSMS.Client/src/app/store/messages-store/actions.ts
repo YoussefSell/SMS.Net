09e2d7b8616e4011eb1779005907574a7e1af74d
Youssef Sellami <contact@youssefsellami.com>
2022-02-23 21:23:01 +0100

    added store action to work with messages

--- src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS.Client/src/app/store/messages-store/actions.ts
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS.Client/src/app/store/messages-store/actions.ts
@@ -1,4 +1,5 @@
 import { createAction, props } from '@ngrx/store';
+import { MessageStatus } from 'src/app/core/constants/enums';
 import { IMessages } from 'src/app/core/models';
 
 /**
@@ -7,13 +8,15 @@ import { IMessages } from 'src/app/core/models';
 export enum StoreActionTypes {
     LOAD_MESSAGES = '@messages/load',
     LOAD_MESSAGES_FINISHED = '@company/load/finished',
-
     SELECT_MESSAGE = '@messages/select',
-    SELECT_MESSAGE_FINISHED = '@messages/select',
+    INSERT_MESSAGE = '@messages/insert',
+    UPDATE_MESSAGE_STATUS = '@messages/update/status',
 }
 
 export const LoadMessages = createAction(StoreActionTypes.LOAD_MESSAGES);
 export const LoadMessagesFinished = createAction(StoreActionTypes.LOAD_MESSAGES_FINISHED, props<{ data: IMessages[] }>());
 
-export const SelectMessages = createAction(StoreActionTypes.LOAD_MESSAGES, props<{ messageId: string; }>());
-export const SelectMessagesFinished = createAction(StoreActionTypes.LOAD_MESSAGES_FINISHED, props<{ messageId: string; data: IMessages }>());
+export const SelectMessage = createAction(StoreActionTypes.LOAD_MESSAGES, props<{ messageId: string; }>());
+
+export const InsertMessage = createAction(StoreActionTypes.INSERT_MESSAGE, props<{ message: IMessages }>());
+export const UpdateMessageStatus = createAction(StoreActionTypes.UPDATE_MESSAGE_STATUS, props<{ messageId: string, newStatus: MessageStatus }>());
--- src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS.Client/src/app/store/messages-store/reducer.ts
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS.Client/src/app/store/messages-store/reducer.ts
@@ -33,4 +33,51 @@ export const MainReducer = createReducer<State>(
             messages: action.data as IMessages[]
         };
     }),
+    on(Actions.SelectMessage, (state, action): State => {
+        const message = state.messages.find(m => m.id == action.messageId);
+
+        // if we found a message set as the selected message.
+        if (message !== null && message !== undefined) {
+            return {
+                ...state,
+                selectedMessage: message,
+            }
+        }
+
+        return state;
+    }),
+    on(Actions.InsertMessage, (state, action): State => {
+        return {
+            ...state,
+            messages: [
+                action.message,
+                ...state.messages
+            ]
+        };
+    }),
+    on(Actions.UpdateMessageStatus, (state, action): State => {
+        // get the message index
+        const messageIndex = state.messages.map(m => m.id).indexOf(action.messageId);
+
+        let newState: State = {
+            ...state,
+            messages: [
+                ...state.messages.slice(0, messageIndex),
+                {
+                    ...state.messages[messageIndex],
+                    status: action.newStatus,
+                },
+                ...state.messages.slice(messageIndex + 1),
+            ]
+        };
+
+        if (state.selectedMessage && state.selectedMessage.id == action.messageId) {
+            newState.selectedMessage = {
+                ...state.selectedMessage,
+                status: action.newStatus,
+            }
+        }
+
+        return newState;
+    }),
 );
