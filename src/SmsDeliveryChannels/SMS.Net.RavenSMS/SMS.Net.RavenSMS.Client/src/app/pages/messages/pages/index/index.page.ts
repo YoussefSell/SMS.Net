7d41de13dda877ee710218e54ab3b20c31940a5d
Youssef Sellami <contact@youssefsellami.com>
2022-02-25 23:12:05 +0100

    removed unused code

--- src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS.Client/src/app/pages/messages/pages/index/index.page.ts
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS.Client/src/app/pages/messages/pages/index/index.page.ts
@@ -17,29 +17,13 @@ import { MessageStatus } from 'src/app/core/constants/enums';
 export class IndexPage implements OnInit, OnDestroy {
   _subSink = new SubSink();
 
-  // Gets a reference to the list element
-  @ViewChild('scheduleList', { static: true }) scheduleList: IonList;
-
-  ios: boolean;
-  dayIndex = 0;
-  queryText = '';
-  segment = 'all';
-  excludeTracks: any = [];
-  showMessages: any = [];
-  groups: any = [];
-  confDate: string;
-  showSearchbar: boolean;
-
+  _is_ios: boolean;
+  _showSearchbar: boolean;
+  _searchQuery: string;
   _messageStatus = MessageStatus;
   _messagesGroups: { date: string; messages: IMessages[] }[] = [];
 
   constructor(
-    public alertCtrl: AlertController,
-    public loadingCtrl: LoadingController,
-    public modalCtrl: ModalController,
-    public router: Router,
-    public routerOutlet: IonRouterOutlet,
-    public toastCtrl: ToastController,
     public config: Config,
     private _store: Store<RootStoreState.State>,
   ) { }
@@ -57,9 +41,7 @@ export class IndexPage implements OnInit, OnDestroy {
         console.log('messages', this._messagesGroups);
       });
 
-    this.updateSchedule();
-
-    this.ios = this.config.get('mode') === 'ios';
+    this._is_ios = this.config.get('mode') === 'ios';
   }
 
   ngOnDestroy(): void {
@@ -70,21 +52,7 @@ export class IndexPage implements OnInit, OnDestroy {
     this._store.dispatch(MessagesStoreActions.DeleteMessage({ messageId: message.id }));
   }
 
-  updateSchedule() {
-    // Close any open sliding items when the schedule updates
-    if (this.scheduleList) {
-      this.scheduleList.closeSlidingItems();
-    }
-
-  }
-
-  async openSocial(network: string, fab: HTMLIonFabElement) {
-    const loading = await this.loadingCtrl.create({
-      message: `Posting to ${network}`,
-      duration: (Math.random() * 1000) + 500
-    });
-    await loading.present();
-    await loading.onWillDismiss();
-    fab.close();
+  search(): void {
+    console.log(this._searchQuery);
   }
 }
