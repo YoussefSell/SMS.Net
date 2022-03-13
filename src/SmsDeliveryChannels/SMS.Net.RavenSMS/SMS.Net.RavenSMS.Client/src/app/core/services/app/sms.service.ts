7ee0e14b6dd3a194a9d4618a80c11c768b323777
Youssef Sellami <contact@youssefsellami.com>
2022-03-13 11:54:54 +0100

    added the sms service logic for sending the messages

--- src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS.Client/src/app/core/services/app/sms.service.ts
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/SMS.Net.RavenSMS.Client/src/app/core/services/app/sms.service.ts
@@ -1,20 +1,47 @@
-// import { SMS } from '@awesome-cordova-plugins/sms/ngx';
+import { SMS } from '@awesome-cordova-plugins/sms/ngx';
 import { Injectable } from '@angular/core';
-declare var SMS: any;
+import { IResult } from '../../models';
 
 @Injectable({ providedIn: 'root' })
 export class SmsService {
-    constructor() { }
+    private sms: SMS;
+
+    constructor() {
+        // there is an issue with DI i couldn't 
+        // inject the SMS instance into the constructor
+        this.sms = new SMS();
+    }
 
     /**
      * send the sms message to the given number
      * @param phoneNumber the phone number to send the message to
      * @param message the sms message content
      */
-    async sendSmsAsync(phoneNumber: string, message: string): Promise<void> {
-        await SMS.send(phoneNumber, message, {
-            android: { intent: '' }
-        });
+    async sendSmsAsync(phoneNumber: string, message: string): Promise<IResult> {
+        try {
+            var result = await this.sms.send(phoneNumber, message, {
+                android: { intent: '' }
+            });
+
+            // check if the sending has succeeded
+            if (result === 'OK') {
+                return {
+                    isSuccess: true,
+                };
+            }
+
+            // the sending has failed
+            return {
+                isSuccess: false,
+                error: 'sending_failed_unknown'
+            }
+        } catch (error) {
+            console.log('failed error', error);
+            return {
+                isSuccess: false,
+                error: 'sending_failed_error'
+            }
+        }
     }
 }
 
