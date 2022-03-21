b1fb7f70ad568b29c2fb112be9581475b1e72625
Youssef Sellami <contact@youssefsellami.com>
2022-03-21 22:21:49 +0100

    added Coravel queue implementation

---
+++ src/SmsDeliveryChannels/SMS.Net.RavenSMS/Queues/SMS.Net.RavenSMS.Queue.Coravel/_global.cs
@@ -0,0 +1,7 @@
+﻿global using Coravel.Invocable;
+global using Coravel.Queuing.Interfaces;
+
+global using SMS.Net.Channel;
+global using SMS.Net.Channel.RavenSMS.Coravel;
+global using SMS.Net.Channel.RavenSMS.Managers;
+global using SMS.Net.Channel.RavenSMS.Entities;
